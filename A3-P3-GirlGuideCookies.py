678e0da1653b818d69b3da15c95a249f38bc504a
krishna-garikapati <w0502117@campus.nscc.ca>
2024-11-19 14:52:36 -0400

    Ready for marking

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -7,15 +7,40 @@
 #Student #:     
 #Student Name:  
 
+def guide(no_of_guides):
+    guide_name_list=[]
+    cookie_box=[]
+    prize=["trip to the Girl Guide Jamboree in Aruba","Super Seller Badge","Left Over Cookies",""]
+    for i in range(no_of_guides):
+        index=i+1
+        guide_name=input("\nEnter the name of guide #{0}: ".format(index))
+        guide_name_list.append(guide_name)
+        boxes_sold=int(input("Enter the number of boxes sold by {0}: ".format(guide_name)))
+        cookie_box.append(boxes_sold)
+    avg=(sum(cookie_box))/(len(cookie_box))
+    print("\nThe average number of boxes sold by each guide was {0}".format(avg))
+
+    print("\nGuide\tPrizes Won:")
+    print("\n------------------------------------------------------------")
+
+    for j in range(len(guide_name_list)):
+        if cookie_box[j]>avg and cookie_box[j]!=max(cookie_box):
+            print("{0}\t-{1}".format(guide_name_list[j],prize[1]))
+        elif cookie_box[j]<=avg and cookie_box[j]!=0:
+            print("{0}\t-{1}".format(guide_name_list[j],prize[2]))
+        elif cookie_box[j]==max(cookie_box):
+            print("{0}\t-{1}".format(guide_name_list[j],prize[0]))
+        else:
+            print("{0}\t-{1}".format(guide_name_list[j],prize[3]))
+
+
+
+
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
+    guides=int(input("Enter the number of guides selling cookies: "))
+    
+    guide(guides)
 
     # YOUR CODE ENDS HERE
 
